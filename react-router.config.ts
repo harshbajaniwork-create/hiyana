16df9b1cabbd5330132bb9868fe832b50b3f3f0b
harshbajani <harshbajani7@gmail.com>
2025-12-26 15:42:11 +0530

    feat: add prerender configuration with a list of static routes

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,23 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  async prerender() {
+    return [
+      "/",
+      "/about",
+      "/services",
+      "/services/sea-freight-fcl",
+      "/services/air-freight",
+      "/services/lcl-consolidation",
+      "/services/iso-tanks",
+      "/services/transportation",
+      "/services/project-cargo",
+      "/services/warehousing",
+      "/services/reefer-container",
+      "/services/custom-clearance",
+      "/services/break-bulk",
+      "/we-export",
+      "/contact",
+    ];
+  },
 } satisfies Config;
